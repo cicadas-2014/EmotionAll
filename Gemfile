7305c6f919a2dd1ef87c91c8b432d37d8920e7e1
newrube7 <newrube7@gmail.com>
2014-07-04 13:24:28 -0400

    Add jasmine gem files

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'spring',        group: :development
 group :development, :test do 
  gem 'rspec-rails' 
  gem 'factory_girl_rails' 
+ gem 'jasmine-rails'
 end
 
 group :test do 
---
+++ spec/map_spec.js
@@ -0,0 +1,12 @@
+// include app/assets/javascripts/high_chart.js
+//= require high_chart
+
+describe("World Map", function() {
+  beforeEach(function() {
+    var Map {} ;
+  });
+  it("is defined", function() {
+    expect(Map).toBeDefined();
+  });
+
+});//end world map
