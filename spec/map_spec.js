f98c126eaa42fe22a1f000768dbe6fd61559d623
newrube7 <newrube7@gmail.com>
2014-07-04 20:15:49 -0400

    Modify jasmine.yml file

--- spec/map_spec.js
+++ spec/map_spec.js
@@ -1,12 +1,21 @@
 // include app/assets/javascripts/high_chart.js
+//require high_chart
+
+// describe("World Map", function() {
+//   beforeEach(function() {
+//     var Map {} ;
+//   });
+//   it("is defined", function() {
+//     expect(Map).toBeDefined();
+//   });
+
+// });//end world map
+
+// include spec/javascripts/helpers/some_helper_file.js and app/assets/javascripts/foo.js
+//= require spec/javascripts/rails_helper.rb
 //= require high_chart
-
-describe("World Map", function() {
-  beforeEach(function() {
-    var Map {} ;
+describe('Foo', function() {
+  it("does something", function() {
+    expect(1 + 1).toBe(2);
   });
-  it("is defined", function() {
-    expect(Map).toBeDefined();
-  });
-
-});//end world map
+});
