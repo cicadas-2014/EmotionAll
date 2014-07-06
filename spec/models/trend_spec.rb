4e2d730563b23182ea3b2edb2e669e73d891ed28
jake <imasidonion@gmail.com>
2014-07-06 15:20:52 -0400

    WIP

--- spec/models/trend_spec.rb
+++ spec/models/trend_spec.rb
@@ -4,5 +4,18 @@ require 'spec_helper'
 describe Trend do
 
   let(:trend) { create :trend }
+  let(:get_twitter_client) { Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.secrets.consumer_key
+      config.consumer_secret     = Rails.application.secrets.consumer_secret
+      config.access_token        = Rails.application.secrets.oauth_token
+      config.access_token_secret = Rails.application.secrets.oauth_token_secret
+    end }
+  let(:woeid) { 23424977 }
+
+  describe "self#get_current_trends" do
+    it "gets new trends from Twitter" do
+      expect(get_twitter_client.trends(woeid))
+    end
+  end
 
 end
