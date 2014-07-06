bca043d6233967c202d6e787247c72f229bdfee2
Danielle Adams <dadams18@gmail.com>
2014-07-06 18:40:23 -0400

    add footer

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,12 +1,13 @@
 function shrinkMenu() {
     window.addEventListener('scroll', function(event) {
-        // var yDistance = window.pageYOffset || document.documentElement.scrollTop;
         var yDistance = 200;
         var y = $(this).scrollTop();
         if (y > yDistance) {
             $('header').addClass('shrink');
+            $('footer').show();
         } else {
             $('header').removeClass('shrink');
+            $('footer').hide();
         }
     })
 }
