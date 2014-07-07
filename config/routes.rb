daf5602764208ccaeb65a5e9412ce8c717460919
jake <imasidonion@gmail.com>
2014-07-07 08:48:07 -0400

    adds about route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/' => 'map#index', as: 'root'
   get '/map/show' => 'map#show'
   get '/trends/:id' => 'trends#json', constraints: {:format => /json/}, as: 'trend'
+  get '/about' => 'pages#about', as: 'about'
 end
