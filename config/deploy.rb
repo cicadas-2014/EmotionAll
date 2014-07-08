0d50526aaffe6736a548aa47e5d558d8769770ac
Kevin Zhou <kz363@nyu.edu>
2014-07-08 10:57:11 -0400

    Remove capistrano

--- config/deploy.rb
+++
@@ -1,2 +0,0 @@
-set :whenever_command, "whenever --set 'consumer_key=$CONSUMER_KEY&consumer_secret=$CONSUMER_SECRET&access_token=$ACCESS_TOKEN&access_token_secret=$ACCESS_TOKEN_SECRET&alchemy_api=$ALCHEMY_API' --update-crontab"
-require "whenever/capistrano"
