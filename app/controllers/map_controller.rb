ec482a927b49fc12893db31eae9593e4ec6f39f4
Danielle Adams & jake <pair+danielleadams.masidonion@devbootcamp.com>
2014-07-08 21:20:03 -0400

    remove unnecessary instance variables for trends

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,8 +1,6 @@
 class MapController < ApplicationController
   def index
     @current_trends = Trend.most_recent
-    @sample_trends = Trend.random_trends
-    @past_trends = Trend.old_trends
   end
 
   def show
