b72c6e8332f106d265a89033aae5b4107f922a5d
Danielle Adams & jake <pair+danielleadams.masidonion@devbootcamp.com>
2014-07-09 21:49:25 -0400

    change hide delay from 50 to 80

--- app/assets/javascripts/highChart.js
+++ app/assets/javascripts/highChart.js
@@ -53,7 +53,7 @@ var Map = {
           + '<span style="font-size:16px; font-weight: bold; line-height: 24px">Tweet Count: </span>'
           + '<span style="font-weight:normal; font-size:16px">{point.tweet_count}</span><br/>',
           footerFormat: '<span style="font-size:8px">Source: AlchemyAPI</span>',
-          hideDelay: 50000000,
+          hideDelay: 80,
           style: {
             font: '"HelveticaNeue", Arial, sans-serif',
             padding: '24px'
