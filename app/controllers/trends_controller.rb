52f9a01e5c7eb344b1ec5febc3c20bd12e591f6f
Danielle Adams & jake <pair+danielleadams.masidonion@devbootcamp.com>
2014-07-09 21:43:52 -0400

    remove tweets method from trends controller

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -3,12 +3,4 @@ class TrendsController < ApplicationController
     map_info = Rails.cache.read('cache').map_info[params[:id].to_i]
     render json: map_info.to_json
   end
-
-  def tweets
-    trend = Trend.find(params[:id])
-    tweet = trend.tweets.where(country_code: params[:country]).sample
-    tweet_from_country = { text: tweet.text,
-                           sentiment: tweet.sentiment_score.round(2) }
-    render json: tweet_from_country.to_json
-  end
 end
