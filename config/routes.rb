1ebde37262ab313a0bd10ec5587198002f2c7557
Danielle Adams & jake <pair+danielleadams.masidonion@devbootcamp.com>
2014-07-09 21:12:20 -0400

    add get tweet for country route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'map#index', as: 'root'
   get '/map/show' => 'map#show'
   get '/trends/:id' => 'trends#json', constraints: {:format => /json/}, as: 'trend'
+  get '/trends/:id/tweets/:country' => 'trends#tweets'
   get '/tweets/' => 'tweets#json', constraints: {:format => /json/}, as: 'tweet'
   get '/about' => 'pages#about', as: 'about'
 end
