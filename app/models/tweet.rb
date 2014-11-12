ea4c661fd9f33aa62dc05713b2a43d23cc1f0f60
Kevin Zhou <kz363@nyu.edu>
2014-11-11 17:43:32 -0800

    Improve performance of averaging Tweet sentiment scores for a Country

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,10 +1,17 @@
 class Tweet < ActiveRecord::Base
 	belongs_to :trend
-
+	belongs_to :country
+	before_create :set_country
+	
 	def analyze_sentiment
 		results = AlchemyAPI.search(:sentiment_analysis, :text => self.text.delete("#").delete("&amp;")) || { type: "neutral", score: 0.0 }
 		self.update_attributes(sentiment: results["type"] || "neutral",
 													 sentiment_score: results["score"] || 0.0)
 	end
 
+	private
+
+	def set_country
+		self.country = Country.find_or_create_by(country_code: self.country_code)
+	end
 end
